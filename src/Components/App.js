8b135e8adf24c4bb1ed48fa8ff6a44111a297f12
P-why1988 <pyguygot.ai113@gmail.com>
2023-09-03 18:49:01 +0200

    urls adapted for gh-pages + visual updated

--- src/Components/App.js
+++ src/Components/App.js
@@ -8,49 +8,54 @@ import MySpecialNavbar from './MySpecialNavbar';
 import { Container } from 'react-bootstrap';
 
 function App() {
-  return (
-    <Container fluid className='p-0 ori-fullsize'>
-        <BrowserRouter>
-            <MySpecialNavbar />
-            <Container fluid className='p-0 ori-body'>
-                <Routes>
-                    <Route exact path='*' element={ <Navigate to='/home' /> } />
-                    <Route exact path='/home' element={ <Home /> } />
-                    <Route 
-                        exact path='/boxes'
-                        element={
-                            <OrigamiPage
-                                type={ TYPE.box }
-                            /> 
-                        }
-                    />
-                    <Route 
-                        exact path='/animals'
-                        element={
-                            <OrigamiPage
-                                type={ TYPE.animal }
-                            /> 
-                        }
-                    />
-                    <Route 
-                        exact path='/other'
-                        element={
-                            <OrigamiPage
-                                type={ TYPE.other }
-                            /> 
-                        }
-                    />
-                    <Route 
-                        exact path='/all'
-                        element={
-                            <OrigamiPage
-                            /> 
-                        }
-                    />
-                </Routes>
-            </Container>
-        </BrowserRouter>
-    </Container>
+    
+    function url(link) {
+        return 'origami-front/' + link;
+    }
+    
+    return (
+        <Container fluid className='p-0 ori-fullsize'>
+            <BrowserRouter>
+                <MySpecialNavbar />
+                <Container fluid className='p-0 ori-body'>
+                    <Routes>
+                        <Route exact path='*' element={ <Navigate to={ url('home') } /> } />
+                        <Route exact path={ url('home') } element={ <Home /> } />
+                        <Route 
+                            exact path={ url('boxes') }
+                            element={
+                                <OrigamiPage
+                                    type={ TYPE.box }
+                                /> 
+                            }
+                        />
+                        <Route 
+                            exact path={ url('animals') }
+                            element={
+                                <OrigamiPage
+                                    type={ TYPE.animal }
+                                /> 
+                            }
+                        />
+                        <Route 
+                            exact path={ url('other') }
+                            element={
+                                <OrigamiPage
+                                    type={ TYPE.other }
+                                /> 
+                            }
+                        />
+                        <Route 
+                            exact path={ url('all') }
+                            element={
+                                <OrigamiPage
+                                /> 
+                            }
+                        />
+                    </Routes>
+                </Container>
+            </BrowserRouter>
+        </Container>
   );
 }
 
--- src/Components/MySpecialNavbar.js
+++ src/Components/MySpecialNavbar.js
@@ -4,9 +4,13 @@ import '../Styles/MySpecialNavbar.css';
 import oriLogo from '../Assets/Icons/logo.png';
 
 function MySpecialNavbar() {
+    function url(link) {
+        return 'origami-front/' + link;
+    }
+
     return (
         <Navbar className='ori-navbar bg-body-tertiary'>
-            <Navbar.Brand as={ Link } to='/home' className='ori-navbar-brand'>
+            <Navbar.Brand as={ Link } to={ url('home') } className='ori-navbar-brand'>
                 <img 
                     src={ oriLogo }
                     alt='Logo Origami'
@@ -15,10 +19,10 @@ function MySpecialNavbar() {
             </Navbar.Brand>
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-                <Nav.Link as={ Link } to='/animals'>Animaux</Nav.Link>
-                <Nav.Link as={ Link } to='/boxes'>Boîtes</Nav.Link>
-                <Nav.Link as={ Link } to='/other'>Autres</Nav.Link>
-                <Nav.Link as={ Link } to='/all'>Tous les modèles</Nav.Link>
+                <Nav.Link as={ Link } to={ url('animals') }>Animaux</Nav.Link>
+                <Nav.Link as={ Link } to={ url('boxes') }>Boîtes</Nav.Link>
+                <Nav.Link as={ Link } to={ url('other') }>Autres</Nav.Link>
+                <Nav.Link as={ Link } to={ url('all') }>Tous les modèles</Nav.Link>
             </Nav>
             </Navbar.Collapse>
         </Navbar>
